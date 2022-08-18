b248a956327a971580a39bd36c786d62cd9d6f2c
Pedro Martins <pedrovrima@gmail.com>
2022-08-18 12:15:00 -0300

    refactor context

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, } from "react"
+import React, { useState, } from "react"
 import getQuestions from "../api/getQuestions";
 import { Questions } from "../api/getQuestions"
 
@@ -8,7 +8,7 @@ export type StoreType = {
     error: boolean;
     answers: boolean[];
     loadQuestions: Function;
-    nextQuestion: (answer:string)=>void;
+    nextQuestion: (answer: string) => void;
     resetContext: Function;
     setError: Function;
 
@@ -16,14 +16,14 @@ export type StoreType = {
 
 
 type StoreProps = {
-    children:  React.ReactNode;
+    children: React.ReactNode;
 }
 
-export const StoreContext = React.createContext<StoreType >({questions:[],error:false,currentQuestionIndex:0,answers:[],loadQuestions:()=>{},nextQuestion:()=>{}, resetContext:()=>{}, setError:()=>{}})
+export const StoreContext = React.createContext<StoreType>({ questions: [], error: false, currentQuestionIndex: 0, answers: [], loadQuestions: () => { }, nextQuestion: () => { }, resetContext: () => { }, setError: () => { } })
 
-export default (props:StoreProps) => {
+export default function ContextStore(props: StoreProps) {
 
-    const {children} = props
+    const { children } = props
 
 
     const [questions, setQuestions] = useState<Questions[]>([])
@@ -38,7 +38,7 @@ export default (props:StoreProps) => {
     const loadQuestions = async (): Promise<void> => {
         console.log("here")
         const loadedQuestions = await getQuestions()
-        if (loadedQuestions.length > 0 ) {
+        if (loadedQuestions.length > 0) {
             setQuestions(loadedQuestions)
         } else {
             console.log("here2")
@@ -49,12 +49,12 @@ export default (props:StoreProps) => {
 
 
     const nextQuestion = (answer: string): void => {
-        setAnswers(answers=>[...answers,answer===questions[currentQuestionIndex].correct_answer])
+        setAnswers(answers => [...answers, answer === questions[currentQuestionIndex].correct_answer])
         setCurrentQuestionIndex((currentQuestionIndex) => currentQuestionIndex + 1)
     }
 
 
-    const resetContext = ():void =>{
+    const resetContext = (): void => {
         setAnswers([])
         setCurrentQuestionIndex(0)
         setQuestions([])
@@ -63,7 +63,7 @@ export default (props:StoreProps) => {
 
     const store = { questions, currentQuestionIndex, error, answers, loadQuestions, nextQuestion, resetContext, setError }
 
-    return (<StoreContext.Provider value= { store } > { children } </StoreContext.Provider>)
+    return (<StoreContext.Provider value={store} > {children} </StoreContext.Provider>)
 
 }
 
