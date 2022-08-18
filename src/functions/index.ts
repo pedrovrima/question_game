f016bcadb4670a1fc1ca8bc5e473a643075546de
Pedro Martins <pedrovrima@gmail.com>
2022-08-18 10:19:06 -0300

    reusable functions

---
+++ src/functions/index.ts
@@ -0,0 +1,15 @@
+
+
+export function answerSignal  (answer: boolean) { return answer ? "+" : "-"}
+
+
+
+// Text received thorugh the API contians HTML entities (e.g. &quot; to reptresent "); 
+// The function parseHtmlEntitites creates a html element and adds the string to it, parsing any entity into the character they represent. 
+// The function then extracts the text from the html element and returns it. 
+
+export function parseHtmlEntities(str:string) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = str;
+    return txt.value;
+}
